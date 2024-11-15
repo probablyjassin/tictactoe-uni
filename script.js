6be3cfb83d1b23c92922b4bb55e090186f787992
Jässin Aouani <aouanijassin@gmail.com>
2024-11-15 12:13:58 +0100

    decide player on start

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-gameContainer = document.querySelector("game")
+// game state
+let isGameRunning = false
 
-console.log("hi")
+// create game board
+gameContainer = document.querySelector("game")
 
 for (let i = 0; i < 9; i++) {
     let newBox = document.createElement('div');
 
     newBox.classList.add('game-box');
 
-    newBox.textContent = ".";
+    newBox.textContent = "⋅";
 
     gameContainer.appendChild(newBox);
 }
+
+// handle game start
+button = document.querySelector("#start-button")
+gameStatus = document.querySelector("#game-status")
+
+players = ["X", "O"]
+activePlayer = players[Math.floor(Math.random() * players.length)];
+
+button.addEventListener("click", function () {
+    isGameRunning = true
+    button.style.display = "none"
+    gameStatus.textContent = `${activePlayer}'s turn!`
+})
