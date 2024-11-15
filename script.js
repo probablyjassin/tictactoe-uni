47dcb870757c1c0edcdd2eb74fd7958447adbee8
Jässin Aouani <aouanijassin@gmail.com>
2024-11-15 13:04:03 +0100

    game logic almost fully complete

--- script.js
+++ script.js
@@ -38,5 +38,58 @@ gameContainer.addEventListener("click", function (event) {
         event.target.classList.add('occupied')
         activePlayer = activePlayer === "X" ? "O" : "X"
         gameStatus.textContent = `${activePlayer}'s turn!`
+
+        let winner = hasWon()
+        if (winner) {
+            gameStatus.textContent = `${winner} has won!`
+            isGameRunning = false
+        }
     }
 })
+
+// check for a winner
+function getSquare(index) {
+    return gameContainer.children[index].innerText
+}
+
+function playerIfIsPlayer(character) {
+    return character == "X" || character == "O" ? character : undefined
+}
+
+function hasWon() {
+
+    // diagonals
+    if (
+        (getSquare(0) === getSquare(4) && getSquare(4) === getSquare(8) && playerIfIsPlayer(getSquare(4))) ||
+        (getSquare(2) === getSquare(4) && getSquare(4) === getSquare(6) && playerIfIsPlayer(getSquare(4)))
+    ) {
+        console.log("diagonal")
+        detection = getSquare(4)
+        if (playerIfIsPlayer(detection)) {
+            return detection
+        }
+    }
+
+    // rows
+    for (let i = 0; i < 9; i = i + 3) {
+        if (getSquare(i) === getSquare(i + 1) === getSquare(i + 2)) {
+            console.log("row")
+            detection = getSquare(i)
+            if (playerIfIsPlayer(detection)) {
+                return detection
+            }
+        }
+    }
+
+    // columns
+    for (let i = 0; i < 3; i++) {
+        if (getSquare(i) === getSquare(i + 3) === getSquare(i + 6)) {
+            console.log("column")
+            detection = getSquare(i)
+            if (playerIfIsPlayer(detection)) {
+                return detection
+            }
+        }
+
+    }
+}
---
+++ test.js
@@ -0,0 +1,16 @@
+let gameArr = [
+    ["0", "1", "2"],
+    ["3", "4", "5"],
+    ["6", "7", "8"]
+]
+
+//console.log(gameArr[2][2])
+
+
+for (let i = 0; i < 3; i++) {
+    console.log(i)
+    console.log(i + 3)
+    console.log(i + 6)
+}
+
+console.log(undefined == undefined) // true
