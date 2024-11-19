c5b440d0f867c1de7933cd6084a87a64113735b7
Jässin Aouani <aouanijassin@gmail.com>
2024-11-19 13:02:04 +0100

    ok

--- networking.js
+++ networking.js
@@ -1,12 +1,30 @@
 const peer = new Peer({
     config: {
         'iceServers': [
-            { urls: 'stun:stun.l.google.com:19302' },
-            { urls: 'stun:stun1.l.google.com:19302' },
-            { urls: 'stun:stun2.l.google.com:19302' },
-            { urls: 'stun:stun3.l.google.com:19302' },
-            { urls: 'stun:stun4.l.google.com:19302' },
-        ]
+            {
+                urls: "stun:stun.relay.metered.ca:80",
+            },
+            {
+                urls: "turn:global.relay.metered.ca:80",
+                username: "e24ca67e21a2a8bc4be55b62",
+                credential: "tK62hvWgO5taVXN6",
+            },
+            {
+                urls: "turn:global.relay.metered.ca:80?transport=tcp",
+                username: "e24ca67e21a2a8bc4be55b62",
+                credential: "tK62hvWgO5taVXN6",
+            },
+            {
+                urls: "turn:global.relay.metered.ca:443",
+                username: "e24ca67e21a2a8bc4be55b62",
+                credential: "tK62hvWgO5taVXN6",
+            },
+            {
+                urls: "turns:global.relay.metered.ca:443?transport=tcp",
+                username: "e24ca67e21a2a8bc4be55b62",
+                credential: "tK62hvWgO5taVXN6",
+            },
+        ],
     }
 });
 let conn = null;
